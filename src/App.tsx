b66c5da94dc32b02a660510dd568b860bc2efa39
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-10-11 00:15:31 +0600

    Added a fixed button to switch between movies and jokes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
 import MovieApp from "./containers/MovieApp/MovieApp.tsx";
+import {useState} from "react";
+import JokeApp from "./containers/JokeApp/JokeApp.tsx";
 
-const App = () => (
-    <>
-        <MovieApp/>
-    </>
-);
+const App = () => {
+    const [page, setPage] = useState(false);
+
+    return (
+        <>
+            <button className="btn btn-outline-dark position-fixed bottom-0 start-50 translate-middle-x mb-3"
+                    onClick={() => setPage(!page)}>
+                {page ? '2 pages' : '1 pages'}
+            </button>
+            {page ? <MovieApp/> : <JokeApp/>}
+        </>
+    );
+};
 
 export default App;
---
+++ src/containers/JokeApp/JokeApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Joke from "../../components/Joke/Joke.tsx";
+
+const JokeApp = () => {
+    return (
+        <div>
+            <Joke/>
+
+        </div>
+    );
+};
+
+export default JokeApp;
